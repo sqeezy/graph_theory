5e632df00ea0f1077a59cc3f0f269d82024efdf5
sqeezy <sqeezy@gmx.de>
2013-12-08 02:23:19 +0100

    Class-diagramm added.

--- graph_theory/Edge.cs
+++ graph_theory/Edge.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace graph_theory
 {
+    /// <summary>
+    /// An Edge which is part of a graph.
+    /// </summary>
     public class Edge
     {
+        private Node _one;
+        private Node _two;
 
+        public Edge(Node one, Node two)
+        {
+            _one = one;
+            _two = two;
+        }
+
+        /// <summary>
+        /// Returns the Node at the opposite end of this edge.
+        /// </summary>
+        /// <param name="start">On end of this Edge.</param>
+        /// <returns>The opposite end.</returns>
+        public Node Next(Node start)
+        {
+            if (start == _one)
+            {
+                return _two;
+            }
+            else if (start == _two)
+            {
+                return _one;
+            }
+            else
+            {
+                throw new ArgumentException("Start node is not positioned at this edge.");
+            }
+        }
     }
 }
--- graph_theory/Node.cs
+++ graph_theory/Node.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace graph_theory
 {
+    /// <summary>
+    /// A Node which is part of a Graph.
+    /// </summary>
     public class Node
     {
+        private
     }
 }
